052c0ffe6fe4f9546f9a78aa8a20c6e4397b0ab8
Sabin Prajapati <sabinprajapati7@gmail.com>
2025-09-07 11:21:46 +0545

    Mailtrap Setup

--- config/settings.py
+++ config/settings.py
@@ -93,9 +93,9 @@ EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
-ADMIN_EMAIL = config("ADMIN_EMAIL")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="webmaster@localhost")
+ADMIN_EMAIL = config("ADMIN_EMAIL", default=None)
 
 
 CORS_ALLOWED_ORIGINS = [
