a627a4afe96d0513860175dbe7497677304f170c
Sabin Prajapati <sabinprajapati7@gmail.com>
2025-09-16 13:05:27 +0545

    ping

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import ContactMessageCreateView, test_email
+from django.http import JsonResponse
+
+
+def ping(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("contact/", ContactMessageCreateView.as_view(), name="contact-create"),
     path("test-email/", test_email, name="test-email"),
+    path("ping/", ping, name="ping"),
 ]
