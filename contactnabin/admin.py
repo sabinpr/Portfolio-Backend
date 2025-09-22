d7dd45c8f86faeba7184de8f765cd464c75ec729
Sabin Prajapati <sabinprajapati7@gmail.com>
2025-09-22 12:16:42 +0545

    neww thing

--- contactnabin/admin.py
+++ contactnabin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "created_at")
+    search_fields = ("name", "email", "message")
+    readonly_fields = ("name", "email", "message", "created_at")
+    ordering = ("-created_at",)
