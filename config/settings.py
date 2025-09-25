5062512f6212bc88b14e5c94f0cd55d940aeab59
Sabin Prajapati <sabinprajapati7@gmail.com>
2025-09-25 23:11:25 +0545

    test

--- config/settings.py
+++ config/settings.py
@@ -119,17 +119,27 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # ------------------------
 # EMAIL
 # ------------------------
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.sendgrid.net")
 EMAIL_PORT = config("EMAIL_PORT", cast=int, default=587)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config(
+    "EMAIL_HOST_USER", default="apikey"
+)  # always 'apikey' for SendGrid
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  # your SendGrid API key
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool, default=False)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+
+
+DEFAULT_FROM_EMAIL = config(
+    "DEFAULT_FROM_EMAIL", default="sabinprajapati.verified@gmail.com"
+)
+
+# Custom admin emails
 ADMIN_EMAIL = config("ADMIN_EMAIL", default=None)
 NABIN_ADMIN_EMAIL = config("NABIN_ADMIN_EMAIL", default=None)
 ISHU_ADMIN_EMAIL = config("ISHU_ADMIN_EMAIL", default=None)
+
 
 # ------------------------
 # CORS
